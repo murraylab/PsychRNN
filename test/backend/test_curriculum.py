4ee57063169cab53539edb20ec1f0fe27852ed42
Jasmine Stone <jasminetstone@gmail.com>
2023-01-19 18:12:31 -0500

    Update test_curriculum.py

--- test/backend/test_curriculum.py
+++ test/backend/test_curriculum.py
@@ -3,9 +3,9 @@ import tensorflow as tf
 from psychrnn.tasks.perceptual_discrimination import PerceptualDiscrimination
 from psychrnn.backend.curriculum import Curriculum
 
-def test_get_generator_function_deprecation():
+def test_get_generator_function_deprecation(capfd):
 	pds = [PerceptualDiscrimination(dt = 10, tau = 100, T = 2000, N_batch = 50, coherence = .7 - i/5) for i in range(4)]
 	curriculum = Curriculum(pds)
-	with pytest.raises(DeprecationWarning) as excinfo:
-		curriculum.get_generator_function()
-	assert 'Curriculum.batch_generator' in str(excinfo.value)
+	curriculum.get_generator_function()
+	out, err = capfd.readouterr()
+	assert 'Curriculum.batch_generator' in out
