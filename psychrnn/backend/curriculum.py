fe56ac0cdb22510eb2ec0f27a68657b47fbf53cb
plhijk <51810139+plhijk@users.noreply.github.com>
2023-01-19 11:40:49 -0500

    fix batch generator in a task (#44)
    * reuse batch generator in a task
    
    * consistent generator method in curriculum
    
    * suggested changes

--- psychrnn/backend/curriculum.py
+++ psychrnn/backend/curriculum.py
@@ -146,10 +146,25 @@ class Curriculum(object):
 		return False
 
 	def get_generator_function(self):
-		""" Return the generator function for the current task.
+		""" Depcreated method to return a generator for the current task. Use :func:`batch_generator` instead. """
 
-		Returns:
-			:func:`psychrnn.tasks.task.Task.batch_generator` function: Task batch generator for the task at the current stage.
-		"""
+		print("WARNING: Curriculum.get_generator_function is deprecated. Use Curriculum.batch_generator instead.")
+		return self.batch_generator()
+
+	def batch_generator(self):
+		""" Returns a generator for the current task.
+
+        Returns:
+            Generator[tuple, None, None]: generator iterator for the current task
+
+        Yields:
+            tuple:
+
+            * **stimulus** (*ndarray(dtype=float, shape =(*:attr:`N_batch`, :attr:`N_steps`, :attr:`N_out` *))*): Task stimuli for :attr:`N_batch` trials.
+            * **target_output** (*ndarray(dtype=float, shape =(*:attr:`N_batch`, :attr:`N_steps`, :attr:`N_out` *))*): Target output for the network on :attr:`N_batch` trials given the :data:`stimulus`.
+            * **output_mask** (*ndarray(dtype=bool, shape =(*:attr:`N_batch`, :attr:`N_steps`, :attr:`N_out` *))*): Output mask for :attr:`N_batch` trials. True when the network should aim to match the target output, False when the target output can be ignored.
+            * **trial_params** (*ndarray(dtype=dict, shape =(*:attr:`N_batch` *,))*): Array of dictionaries containing the trial parameters produced by :func:`generate_trial_params` for each trial in :attr:`N_batch`.
+        
+        """
 		return self.tasks[self.stage].batch_generator()
 
--- psychrnn/tasks/task.py
+++ psychrnn/tasks/task.py
@@ -52,6 +52,9 @@ class Task(ABC):
         self.alpha = (1.0 * self.dt) / self.tau
         self.N_steps = int(np.ceil(self.T / self.dt))
 
+        # Initialize the generator used by get_trial_batch
+        self._batch_generator = self.batch_generator()
+
     def get_task_params(self):
         """ Get dictionary of task parameters.
 
@@ -182,7 +185,7 @@ class Task(ABC):
         return x_data, y_data, mask
 
     def batch_generator(self):
-        """ Generates a batch of trials.
+        """ Returns a generator for this task.
 
         Returns:
             Generator[tuple, None, None]:
@@ -225,7 +228,7 @@ class Task(ABC):
     def get_trial_batch(self):
         """Get a batch of trials.
 
-        Wrapper for :code:`next(self.batch_generator())`.
+        Wrapper for :code:`next(self._batch_generator)`.
 
         Returns:
             tuple:
@@ -236,5 +239,5 @@ class Task(ABC):
             * **trial_params** (*ndarray(dtype=dict, shape =(*:attr:`N_batch` *,))*): Array of dictionaries containing the trial parameters produced by :func:`generate_trial_params` for each trial in :attr:`N_batch`.
 
         """
-        return next(self.batch_generator())
+        return next(self._batch_generator)
 
