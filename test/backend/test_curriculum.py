6f6ac446c702405e4bcd5acb30762350be6b8787
Jasmine Stone <jasminetstone@gmail.com>
2023-01-19 16:59:04 -0500

    Update test_curriculum.py
    import curriculum

--- test/backend/test_curriculum.py
+++ test/backend/test_curriculum.py
@@ -1,6 +1,7 @@
 import pytest
 import tensorflow as tf
 from psychrnn.tasks.perceptual_discrimination import PerceptualDiscrimination
+from psychrnn.backend.curriculum import Curriculum
 
 def test_get_generator_function_deprecation():
 	pds = [PerceptualDiscrimination(dt = 10, tau = 100, T = 2000, N_batch = 50, coherence = .7 - i/5) for i in range(4)]
