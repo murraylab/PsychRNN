f2222b7f40376b3f4f9116de13924587b1032028
Jasmine Stone <jasminetstone@gmail.com>
2023-01-19 17:32:12 -0500

    Update test_curriculum.py

--- test/backend/test_curriculum.py
+++ test/backend/test_curriculum.py
@@ -7,5 +7,5 @@ def test_get_generator_function_deprecation():
 	pds = [PerceptualDiscrimination(dt = 10, tau = 100, T = 2000, N_batch = 50, coherence = .7 - i/5) for i in range(4)]
 	curriculum = Curriculum(pds)
 	with pytest.raises(DeprecationWarning) as excinfo:
-		pds.get_generator_function()
+		curriculum.get_generator_function()
 	assert 'Curriculum.batch_generator' in str(excinfo.value)
