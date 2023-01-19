b8e7f396651a6ff312a8e32f9f0c90bef6d6c132
Jasmine Stone <jasminetstone@gmail.com>
2023-01-19 12:27:51 -0500

    Add test for warning

---
+++ test/backend/test_curriculum.py
@@ -0,0 +1,11 @@
+import pytest
+import tensorflow as tf
+from psychrnn.tasks.perceptual_discrimination import PerceptualDiscrimination
+
+def test_get_generator_function_deprecation():
+  pds = [PerceptualDiscrimination(dt = 10, tau = 100, T = 2000, N_batch = 50, coherence = .7 - i/5) for i in range(4)]
+  curriculum = Curriculum(pds)
+  
+  with pytest.raises(DeprecationWarning) as excinfo:
+    pds.get_generator_function()
+	assert 'Curriculum.batch_generator' in str(excinfo.value)
