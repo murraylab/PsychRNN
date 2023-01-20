4143c37a7f3deb86393b7f8a25dbe4abf2caa51f
Jasmine Stone <jasminetstone@gmail.com>
2023-01-20 10:01:54 -0500

    Update test_curriculum.py

--- test/backend/test_curriculum.py
+++ test/backend/test_curriculum.py
@@ -6,6 +6,5 @@ from psychrnn.backend.curriculum import Curriculum
 def test_get_generator_function_deprecation(capfd):
 	pds = [PerceptualDiscrimination(dt = 10, tau = 100, T = 2000, N_batch = 50, coherence = .7 - i/5) for i in range(4)]
 	curriculum = Curriculum(pds)
-	curriculum.get_generator_function()
-	out, err = capfd.readouterr()
-	assert 'Curriculum.batch_generator' in err
+	with pytest.deprecated_call():
+		curriculum.get_generator_function()
